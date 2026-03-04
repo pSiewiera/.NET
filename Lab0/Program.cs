7c55464c4e5fabaf4a8c87f951c7b035761210f9
pSiewiera <pawel.k.siewiera@gmail.com>
2026-03-04 17:51:06 +0100

    Wykonanie zadania

---
+++ Lab0/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz fb = new FizzBuzz (40);
+            fb.Print();
+        }
+    }
+     internal class FizzBuzz
+    {
+        private int maxNumber;
+        public FizzBuzz(int maxNumber)
+        {
+            this.maxNumber = maxNumber;
+        }
+        public void Print()
+        {
+            for (int i=1; i <=maxNumber; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 ==0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
