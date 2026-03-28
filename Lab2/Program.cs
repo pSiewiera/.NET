8e14cb00b8d364efec0653c1e77a2ef783fc2f37
pSiewiera <pawel.k.siewiera@gmail.com>
2026-03-28 20:51:44 +0100

    Wykoanie zadania 1 w laboratorium 2

---
+++ Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+string url = $"https://api.open-meteo.com/v1/forecast?latitude=51.1079&longitude=17.0385&current_weather=true&relative_humidity=true";
+using HttpClient client = new();
+
+string json = await client.GetStringAsync(url);
+
+WeatherData data = JsonSerializer.Deserialize<WeatherData>(json);
+
+var w = data.current_weather;
+Console.WriteLine($"Temperature: {w.temperature}C");
+Console.WriteLine($"Wind Speed: {w.windspeed} km/h");
+Console.WriteLine($"Wind Direction: {w.winddirection}");
+Console.WriteLine($"Time: {w.time}");
+
+public class CurrentWeather
+{
+    public double temperature { get; set; }
+    public double windspeed { get; set; }
+    public int winddirection { get; set; }
+    public string time { get; set; }
+}
+public class WeatherData
+{
+    public CurrentWeather current_weather { get; set; }
+}
